c8ed3db40c844872e59333111b61749d9b67c26e
Edo Pranata <edo.pranata@gmail.com>
2022-04-14 22:19:25 +0700

    Fixed error when open report transaction

--- resources/views/pages/reporting/transaction/report-transaction.blade.php
+++ resources/views/pages/reporting/transaction/report-transaction.blade.php
@@ -91,14 +91,6 @@
                             <td>{{ $sell['status'] }}</td>
                         </tr>
                             @if($sell['returns'])
-{{--                                <tr class="border-0">--}}
-{{--                                    <td colspan="3"></td>--}}
-{{--                                    <td class="text-bold">Nama Produk Retur</td>--}}
-{{--                                    <td class="text-right text-bold">Quantity retur</td>--}}
-{{--                                    <td class="text-right text-bold">Harga Produk retur</td>--}}
-{{--                                    <td class="text-right text-bold">Total harga</td>--}}
-{{--                                    <td colspan="3"></td>--}}
-{{--                                </tr>--}}
                                 @foreach($sell['returns'] as $index => $return)
                                     <tr class="border-0">
                                         <td colspan="3"></td>
@@ -117,19 +109,19 @@
                         <tfoot>
                         <tr>
                             <th colspan="4" class="text-right">Total Penjualan</th>
-                            <th class="text-right">Rp. {{ number_format($sells->sum('bill')) }}</th>
-                            <th class="text-right">Rp. {{ number_format($sells->sum('discount')) }}</th>
-                            <th class="text-right">Rp. {{ number_format($sells->sum('total')) }}</th>
+                            <th class="text-right">Rp. {{ number_format($sells ? $sells->sum('bill') : 0) }}</th>
+                            <th class="text-right">Rp. {{ number_format($sells ? $sells->sum('discount') : 0) }}</th>
+                            <th class="text-right">Rp. {{ number_format($sells ? $sells->sum('total') : 0) }}</th>
                             <th colspan="3"></th>
                         </tr>
                         <tr>
                             <th colspan="4" class="text-right">Total retur</th>
-                            <th colspan="3" class="text-right">Rp. {{ number_format($sells->sum('sum_return')) }}</th>
+                            <th colspan="3" class="text-right">Rp. {{ number_format($sells ? $sells->sum('sum_return') : 0) }}</th>
                             <th colspan="3"></th>
                         </tr>
                         <tr>
                             <th colspan="4" class="text-right">Total Keseluruhan</th>
-                            <th colspan="3" class="text-right">Rp. {{ number_format($sells->sum('total') - $sells->sum('sum_return')) }}</th>
+                            <th colspan="3" class="text-right">Rp. {{ number_format(($sells ? $sells->sum('total') : 0) - ($sells ? $sells->sum('sum_return') : 0)) }}</th>
                             <th colspan="3"></th>
                         </tr>
                         </tfoot>
@@ -139,7 +131,7 @@
             @endif
         </div>
         <div class="col-lg-12">
-            @if($returns->count() > 0)
+            @if($returns)
                 <div class="card">
                     <div class="card-header">
                         Transaksi Retur {{ $transaction_date }}
@@ -176,7 +168,7 @@
                             <tfoot>
                             <tr>
                                 <th colspan="7" class="text-right">Total Retur</th>
-                                <th class="text-right">Rp. {{ number_format($returns->sum('total')) }}</th>
+                                <th class="text-right">Rp. {{ number_format($returns ? $returns->sum('total') : 0) }}</th>
                                 <th></th>
                             </tr>
                             </tfoot>
@@ -186,7 +178,8 @@
             @endif
         </div>
     </div>
-    <div class="row">
+    @if($sells)
+        <div class="row">
         <div class="col-md-6">
 
         </div>
@@ -205,14 +198,15 @@
                         <dt class="col-sm-4">Kasir</dt>
                         <dd class="col-sm-8">{{ \auth()->user()->username }}</dd>
                         <dt class="col-sm-4">Total Penjualan</dt>
-                        <dd class="col-sm-8 text-bold">Rp. {{ number_format($sells->sum('total') - $sells->sum('sum_return')) }}</dd>
+                        <dd class="col-sm-8 text-bold">Rp. {{ number_format($sells ? $sells->sum('total') - $sells->sum('sum_return') : 0) }}</dd>
                         <dt class="col-sm-4">Total Retur Barang</dt>
-                        <dd class="col-sm-8 text-bold">Rp. {{ number_format($returns->sum('total')) }}</dd>
+                        <dd class="col-sm-8 text-bold">Rp. {{ number_format($returns ? $returns->sum('total') : 0) }}</dd>
                         <dt class="col-sm-4">Subtotal</dt>
-                        <dd class="col-sm-8 text-bold">Rp. {{ number_format(($sells->sum('total') - $sells->sum('sum_return')) - $returns->sum('total')) }}</dd>
+                        <dd class="col-sm-8 text-bold">Rp. {{ number_format(($sells ? $sells->sum('total') - $sells->sum('sum_return') : 0) - ($returns ? $returns->sum('total') : 0)) }}</dd>
                     </dl>
                 </div>
             </div>
         </div>
     </div>
+    @endif
 </div>
