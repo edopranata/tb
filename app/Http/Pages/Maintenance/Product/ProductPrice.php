fdda69c853496d7979f49030256401a5c592ee8d
Edo Pranata <edo.pranata@gmail.com>
2022-06-05 17:34:08 +0700

    Adding Maintenance Product

---
+++ app/Http/Pages/Maintenance/Product/ProductPrice.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Pages\Maintenance\Product;
+
+use Livewire\Component;
+
+class ProductPrice extends Component
+{
+    
+    public function render()
+    {
+        return view('pages.maintenance.product.product-price');
+    }
+
+    public function save()
+    {
+        
+    }
+}
